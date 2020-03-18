157c0350fcb698be03f72cd01948f81c5848f26a
jbadelson <jeff.adelson@gmail.com>
2020-03-18 10:20:57 -0500

    Fix date formatting issue

--- covid_la.py
+++ covid_la.py
@@ -42,16 +42,18 @@ def la_covid(parish_url, state_url, date):
     case_file = pd.read_csv('data/cases.csv', dtype = {'FIPS' : object})
     if date in case_file.columns:
         case_file = case_file.drop(columns = date)
+    print(case_file)
+    print(date)
     case_file.merge(cases[['FIPS', date]], 
                     on='FIPS', 
-                    how='outer').to_csv('data/cases.csv', index=False)
+                    how='outer').fillna(0).to_csv('data/cases.csv', index=False)
     deaths = deaths.rename(columns = {'Deaths' : date, 'PFIPS' : 'FIPS'})
     death_file = pd.read_csv('data/deaths.csv', dtype = {'FIPS' : object})
     if date in death_file.columns:
         death_file = death_file.drop(columns = date)
     death_file.merge(deaths[['FIPS', date]],
-                     on='FIPS',
-                     how='outer').to_csv('data/deaths.csv', index=False)
+                      on='FIPS',
+                      how='outer').fillna(0).to_csv('data/deaths.csv', index=False)
     state = pd.DataFrame(esri_cleaner(la_state_url))
     tests = state[state['Category'] == 'Test Completed'].rename(columns = ({'Value' : date}))
     tests['FIPS'] = 22
@@ -62,14 +64,14 @@ def la_covid(parish_url, state_url, date):
         test_file = test_file.drop(columns = date)
     test_file.merge(tests[['FIPS', date]], 
                     on='FIPS', 
-                    how='outer').to_csv('data/tests.csv', index=False)
+                    how='outer').fillna(0).to_csv('data/tests.csv', index=False)
     ages = state[state['Category'] != 'Test Completed'].rename(columns=({'Value' : date}))
     age_file = pd.read_csv('data/ages.csv')
     if date in age_file.columns:
         age_file = age_file.drop(columns = date)
     age_file.merge(ages[['Category', date]], on='Category', how='outer').to_csv('data/ages.csv', index=False)
 
-update_date = datetime.now().strftime('%Y-%m-%d')
+update_date = datetime.now().strftime('%m/%d/%Y')
 
 la_state_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/State_Level_Information_1/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
 la_county_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/Cases_by_Parish_1/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
