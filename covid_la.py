249709ec092a7be3e53be87beea901ed667e6a7a
jbadelson <jeff.adelson@gmail.com>
2020-03-17 22:40:29 -0500

    Initial commit.

---
+++ covid_la.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 19:30:07 2020
+
+@author: jeff
+"""
+
+import pandas as pd
+import json
+from urllib.request import urlopen
+from datetime import datetime
+
+def esri_cleaner(url):
+    data = urlopen(url).read()
+    raw_json = json.loads(data)
+    formatted_json = [feature['attributes'] for feature in raw_json['features']]
+    return formatted_json
+
+def la_covid(parish_url, state_url, date):
+    cases = pd.DataFrame(esri_cleaner(parish_url))
+    cases = cases.rename(columns = {'Cases' : date, 'PFIPS' : 'FIPS'})
+    case_file = pd.read_csv('out/cases.csv', dtype = {'FIPS' : object})
+    if date in case_file.columns:
+        case_file = case_file.drop(columns = date)
+    case_file.merge(cases[['FIPS', date]], on='FIPS').to_csv('out/cases.csv', index=False)
+    state = pd.DataFrame(esri_cleaner(la_state_url))
+    tests = state[state['Category'] == 'Test Completed'].rename(columns = ({'Value' : date}))
+    tests['FIPS'] = 22
+    test_file = pd.read_csv('out/tests.csv')
+#    print(test_file)
+#    print(tests)
+    if date in test_file.columns:
+        test_file = test_file.drop(columns = date)
+    test_file.merge(tests[['FIPS', date]], on='FIPS').to_csv('out/tests.csv', index=False)
+    ages = state[state['Category'] != 'Test Completed'].rename(columns=({'Value' : date}))
+    age_file = pd.read_csv('out/ages.csv')
+    print(ages)
+    print(age_file)
+    if date in age_file.columns:
+        age_file = age_file.drop(columns = date)
+    age_file.merge(ages[['Category', date]], on='Category', how='outer').to_csv('out/ages.csv', index=False)
+
+update_date = datetime.now().strftime('%Y-%m-%d')
+
+la_state_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/State_Level_Information_1/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
+la_county_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/Cases_by_Parish_1/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
+la_covid(la_county_url, la_state_url, update_date)
