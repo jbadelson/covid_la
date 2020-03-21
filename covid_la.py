8e592b344807a39d8c31f7e873b1c779dcf9a70c
jbadelson <jeff.adelson@gmail.com>
2020-03-20 23:31:04 -0500

    3/20/2020 update, added new demo info

--- covid_la.py
+++ covid_la.py
@@ -35,42 +35,49 @@ def esri_cleaner(url):
     return formatted_json
 
 def la_covid(parish_url, state_url, date):
-    cases = pd.DataFrame(esri_cleaner(parish_url))
-    deaths = cases.copy()
-    cases = cases.rename(columns = {'Cases' : date, 'PFIPS' : 'FIPS'})
-    cases.loc[cases['PARISH'] == 'Parish Under Investigation', 'FIPS'] = '22999'
-    case_file = pd.read_csv('data/cases.csv', dtype = {'FIPS' : object})
-    if date in case_file.columns:
-        case_file = case_file.drop(columns = date)
-    case_file.merge(cases[['FIPS', date]], 
-                    on='FIPS', 
-                    how='outer').fillna(0).to_csv('data/cases.csv', index=False)
-    deaths = deaths.rename(columns = {'Deaths' : date, 'PFIPS' : 'FIPS'})
-    death_file = pd.read_csv('data/deaths.csv', dtype = {'FIPS' : object})
-    if date in death_file.columns:
-        death_file = death_file.drop(columns = date)
-    death_file.merge(deaths[['FIPS', date]],
-                      on='FIPS',
-                      how='outer').fillna(0).to_csv('data/deaths.csv', index=False)
-    state = pd.DataFrame(esri_cleaner(la_state_url))
-    tests = state[state['Category'] == 'Test Completed'].rename(columns = ({'Value' : date}))
-    tests['FIPS'] = 22
-    test_file = pd.read_csv('data/tests.csv')
-#    print(test_file)
-#    print(tests)
-    if date in test_file.columns:
-        test_file = test_file.drop(columns = date)
-    test_file.merge(tests[['FIPS', date]], 
-                    on='FIPS', 
-                    how='outer').fillna(0).to_csv('data/tests.csv', index=False)
-    ages = state[state['Category'] != 'Test Completed'].rename(columns=({'Value' : date}))
-    age_file = pd.read_csv('data/ages.csv')
-    if date in age_file.columns:
-        age_file = age_file.drop(columns = date)
-    age_file.merge(ages[['Category', date]], on='Category', how='outer').to_csv('data/ages.csv', index=False)
+     cases = pd.DataFrame(esri_cleaner(parish_url))
+     deaths = cases.copy()
+     cases = cases.rename(columns = {'Cases' : date, 'PFIPS' : 'FIPS'})
+     cases.loc[cases['FIPS'] == '0', 'FIPS'] = '22000'
+     case_file = pd.read_csv('data/cases.csv', dtype = {'FIPS' : object})
+     if date in case_file.columns:
+         case_file = case_file.drop(columns = date)
+     case_file.merge(cases[['FIPS', date]], 
+                     on='FIPS', 
+                     how='outer').fillna(0).to_csv('data/cases.csv', index=False)
+     deaths = deaths.rename(columns = {'Deaths' : date, 'PFIPS' : 'FIPS'})
+     death_file = pd.read_csv('data/deaths.csv', dtype = {'FIPS' : object})
+     if date in death_file.columns:
+         death_file = death_file.drop(columns = date)
+     death_file.merge(deaths[['FIPS', date]],
+                       on='FIPS',
+                       how='outer').fillna(0).to_csv('data/deaths.csv', index=False)
+     state = pd.DataFrame(esri_cleaner(la_state_url))
+     tests = state[state['Category'] == 'Test Completed'].rename(columns = ({'Value' : 'Public', 'Value2' : 'Private'}))
+     tests['date'] = update_date
+     tests = tests[['date', 'Public', 'Private']].set_index('date').transpose().reset_index().rename(columns={'index' : 'Category'})
+     test_file = pd.read_csv('data/tests.csv')
+     if date in test_file.columns:
+         test_file = test_file.drop(columns = date)
+     test_file.merge(tests[['Category', date]], 
+                     on='Category', 
+                     how='outer').fillna(0).to_csv('data/tests.csv', index=False)
+     ages = state[state['Category'] != 'Test Completed'].rename(columns=({'Value' : date}))
+     age_file = pd.read_csv('data/ages.csv')
+     if date in age_file.columns:
+         age_file = age_file.drop(columns = date)
+     age_file.merge(ages[['Category', date]], on='Category', how='outer').to_csv('data/ages.csv', index=False)
+     death_demo = state[state['Category'] != 'Test Completed'].rename(columns=({'Value2' : date}))
+     death_demo_file = pd.read_csv('data/death_demo.csv')
+     if date in death_demo_file.columns:
+         death_demo_file = death_demo_file.drop(columns = date)
+     death_demo_file.merge(death_demo[['Category', date]], on='Category', how='outer').to_csv('data/death_demo.csv', index=False)
 
-update_date = datetime.now().strftime('%m/%d/%Y')
 
-la_state_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/State_Level_Information_1/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
-la_county_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/Cases_by_Parish_1/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
-la_covid(la_county_url, la_state_url, update_date)
+update_date = '{d.month}/{d.day}/{d.year}'.format(d=datetime.now())
+
+la_state_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/State_Level_Information_2/FeatureServer/0/query?where=1%3D1&outFields=*&f=pjson'
+la_county_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/Cases_by_Parish_1/FeatureServer/0/query?where=1%3D1&outFields=*&f=pjson'
+
+
+la_covid(la_county_url,la_state_url, update_date)
