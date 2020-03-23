474730c14f36519f25a8dc695e4cccaf3198a243
jbadelson <jeff.adelson@gmail.com>
2020-03-23 16:45:17 -0500

    3/23/20 update

--- covid_la.py
+++ covid_la.py
@@ -35,45 +35,59 @@ def esri_cleaner(url):
     return formatted_json
 
 def la_covid(parish_url, state_url, date):
-     cases = pd.DataFrame(esri_cleaner(parish_url))
-     cases = cases.rename(columns = {'Cases' : date, 'PFIPS' : 'FIPS'})
-     cases.loc[cases['FIPS'] == '0', 'FIPS'] = '22000'
-     deaths = cases.copy()
-     case_file = pd.read_csv('data/cases.csv', dtype = {'FIPS' : object})
-     if date in case_file.columns:
-         case_file = case_file.drop(columns = date)
-     case_file.merge(cases[['FIPS', date]], 
-                     on='FIPS', 
-                     how='outer').to_csv('data/cases.csv', index=False)
-     deaths = deaths.rename(columns = {'Deaths' : date, 'PFIPS' : 'FIPS'})
-     deaths.to_csv('c:/users/jeff/desktop/deaths.csv')
-     death_file = pd.read_csv('data/deaths.csv', dtype = {'FIPS' : object})
-     death_file.to_csv('c:/users/jeff/desktop/death_file.csv')
-     if date in death_file.columns:
-         death_file = death_file.drop(columns = date)
-     death_file.merge(deaths[['FIPS', date]],
-                       on='FIPS',
-                       how='outer').to_csv('data/deaths.csv', index=False)
-     state = pd.DataFrame(esri_cleaner(la_state_url))
-     tests = state[state['Category'] == 'Test Completed'].rename(columns = ({'Value' : 'Public', 'Value2' : 'Private'}))
-     tests['date'] = update_date
-     tests = tests[['date', 'Public', 'Private']].set_index('date').transpose().reset_index().rename(columns={'index' : 'Category'})
-     test_file = pd.read_csv('data/tests.csv')
-     if date in test_file.columns:
-         test_file = test_file.drop(columns = date)
-     test_file.merge(tests[['Category', date]], 
-                     on='Category', 
-                     how='outer').to_csv('data/tests.csv', index=False)
-     case_demo = state[state['Category'] != 'Test Completed'].rename(columns=({'Value' : date}))
-     case_demo_file = pd.read_csv('data/case_demo.csv')
-     if date in case_demo_file.columns:
-         case_demo_file = case_demo_file.drop(columns = date)
-     case_demo_file.merge(case_demo[['Category', date]], on='Category', how='outer').to_csv('data/case_demo.csv', index=False)
-     death_demo = state[state['Category'] != 'Test Completed'].rename(columns=({'Value2' : date}))
-     death_demo_file = pd.read_csv('data/death_demo.csv')
-     if date in death_demo_file.columns:
-         death_demo_file = death_demo_file.drop(columns = date)
-     death_demo_file.merge(death_demo[['Category', date]], on='Category', how='outer').to_csv('data/death_demo.csv', index=False)
+    cases = pd.DataFrame(esri_cleaner(parish_url))
+    cases = cases.rename(columns = {'PFIPS' : 'FIPS'})
+    cases.loc[cases['FIPS'] == '0', 'FIPS'] = '22000'
+    deaths = cases.copy()
+    cases = cases.rename(columns = {'Cases' : date})
+    print('Case fields:')
+    for c in cases.columns:
+        print(c)
+    case_file = pd.read_csv('data/cases.csv', dtype = {'FIPS' : object})
+    if date in case_file.columns:
+        case_file = case_file.drop(columns = date)
+    case_file.merge(cases[['FIPS', date]], 
+                    on='FIPS', 
+                    how='outer').to_csv('data/cases.csv', index=False)
+    deaths = deaths.rename(columns = {'Deaths' : date, 'PFIPS' : 'FIPS'})
+    print('Death fields:')
+    for d in deaths.columns:
+        print(d)
+    death_file = pd.read_csv('data/deaths.csv', dtype = {'FIPS' : object})
+    if date in death_file.columns:
+        death_file = death_file.drop(columns = date)
+    death_file.merge(deaths[['FIPS', date]],
+                      on='FIPS',
+                      how='outer').to_csv('data/deaths.csv', index=False)
+    state = pd.DataFrame(esri_cleaner(la_state_url))
+    tests = state[state['Category'] == 'Test Completed'].rename(columns = ({'Value' : 'Public', 'Value2' : 'Private'}))
+    tests['date'] = update_date
+    tests = tests[['date', 'Public', 'Private']].set_index('date').transpose().reset_index().rename(columns={'index' : 'Category'})
+    print('Test fields:')
+    for t in tests.columns:
+        print(t)
+    test_file = pd.read_csv('data/tests.csv')
+    if date in test_file.columns:
+        test_file = test_file.drop(columns = date)
+    test_file.merge(tests[['Category', date]], 
+                    on='Category', 
+                    how='outer').to_csv('data/tests.csv', index=False)
+    case_demo = state[state['Category'] != 'Test Completed'].rename(columns=({'Value' : date}))
+    print('Case Demo fields:')
+    for cd in case_demo.columns:
+        print(cd)
+    case_demo_file = pd.read_csv('data/case_demo.csv')
+    if date in case_demo_file.columns:
+        case_demo_file = case_demo_file.drop(columns = date)
+    case_demo_file.merge(case_demo[['Category', date]], on='Category', how='outer').to_csv('data/case_demo.csv', index=False)
+    death_demo = state[state['Category'] != 'Test Completed'].rename(columns=({'Value2' : date}))
+    print('Death Demo Fields:')
+    for dd in death_demo:
+        print(dd)
+    death_demo_file = pd.read_csv('data/death_demo.csv')
+    if date in death_demo_file.columns:
+        death_demo_file = death_demo_file.drop(columns = date)
+    death_demo_file.merge(death_demo[['Category', date]], on='Category', how='outer').to_csv('data/death_demo.csv', index=False)
 
 
 update_date = '{d.month}/{d.day}/{d.year}'.format(d=datetime.now())
