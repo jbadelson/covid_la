ee41b0c9814a582dee8d58c4614397e5103f1520
jbadelson <jeff.adelson@gmail.com>
2020-03-30 22:44:15 -0500

    Add in parish test details.

--- covid_la.py
+++ covid_la.py
@@ -41,6 +41,7 @@ def la_covid(parish_url, state_url, date):
     cases = cases.rename(columns = {'PFIPS' : 'FIPS'})
     cases.loc[cases['FIPS'] == '0', 'FIPS'] = '22000'
     deaths = cases.copy()
+    tests_detail = cases.copy()
     cases = cases.rename(columns = {'Cases' : date})
     case_file = pd.read_csv('data/cases.csv', dtype = {'FIPS' : object})
     if date in case_file.columns:
@@ -49,22 +50,30 @@ def la_covid(parish_url, state_url, date):
                     on='FIPS', 
                     how='outer').to_csv('data/cases.csv', index=False)
     deaths = deaths.rename(columns = {'Deaths' : date, 'PFIPS' : 'FIPS'})
-    print('Death fields:')
-    for d in deaths.columns:
-        print(d)
     death_file = pd.read_csv('data/deaths.csv', dtype = {'FIPS' : object})
     if date in death_file.columns:
         death_file = death_file.drop(columns = date)
     death_file.merge(deaths[['FIPS', date]],
                       on='FIPS',
                       how='outer').to_csv('data/deaths.csv', index=False)
+    tests_detail_file = pd.read_csv('data/tests_detail.csv', dtype={'FIPS' : object})
+    if date in tests_detail_file.columns:
+        tests_detail_file = tests_detail_file.drop(columns = date)
+    tests_public = tests_detail[['FIPS', 'State_Tests']]
+    tests_public = tests_public.rename(columns = {'State_Tests' : date, 'PFIPS' : 'FIPS'})
+    tests_public['Category'] = 'Public'
+    tests_private = tests_detail[['FIPS', 'Commercial_Tests']]
+    tests_private = tests_private.rename(columns = {'Commercial_Tests' : date, 'PFIPS' : 'FIPS'})
+    tests_private['Category'] = 'Private'
+    tests = tests_public.append(tests_private)
+    tests_detail_file.merge(tests, 
+                            left_on=['FIPS', 'Category'], 
+                            right_on=['FIPS', 'Category'], 
+                            how='outer').to_csv('data/tests_detail.csv', index=False)
     state = pd.DataFrame(esri_cleaner(la_state_url))
     tests = state[state['Category'] == 'Test Completed'].rename(columns = ({'Value' : 'Public', 'Value2' : 'Private'}))
     tests['date'] = update_date
     tests = tests[['date', 'Public', 'Private']].set_index('date').transpose().reset_index().rename(columns={'index' : 'Category'})
-    print('Test fields:')
-    for t in tests.columns:
-        print(t)
     test_file = pd.read_csv('data/tests.csv')
     if date in test_file.columns:
         test_file = test_file.drop(columns = date)
@@ -72,17 +81,11 @@ def la_covid(parish_url, state_url, date):
                     on='Category', 
                     how='outer').to_csv('data/tests.csv', index=False)
     case_demo = state[state['Category'] != 'Test Completed'].rename(columns=({'Value' : date}))
-    print('Case Demo fields:')
-    for cd in case_demo.columns:
-        print(cd)
     case_demo_file = pd.read_csv('data/case_demo.csv')
     if date in case_demo_file.columns:
         case_demo_file = case_demo_file.drop(columns = date)
     case_demo_file.merge(case_demo[['Category', date]], on='Category', how='outer').to_csv('data/case_demo.csv', index=False)
     death_demo = state[state['Category'] != 'Test Completed'].rename(columns=({'Value2' : date}))
-    print('Death Demo Fields:')
-    for dd in death_demo:
-        print(dd)
     death_demo_file = pd.read_csv('data/death_demo.csv')
     if date in death_demo_file.columns:
         death_demo_file = death_demo_file.drop(columns = date)
@@ -91,8 +94,8 @@ def la_covid(parish_url, state_url, date):
 
 update_date = '{d.month}/{d.day}/{d.year}'.format(d=datetime.now())
 
-la_state_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/State_Level_Information_2/FeatureServer/0/query?where=1%3D1&outFields=*&f=pjson'
-la_county_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/Cases_by_Parish_1/FeatureServer/0/query?where=1%3D1&outFields=*&f=pjson'
+la_state_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/State_Level_Information_2/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=pjson'
+la_county_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/ArcGIS/rest/services/Cases_by_Parish/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=pjson'
 
 
 la_covid(la_county_url,la_state_url, update_date)
