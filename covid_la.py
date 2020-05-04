08868ad7fe1a39187b392e35864789aa723e51e8
jbadelson <jeff.adelson@gmail.com>
2020-05-04 12:04:48 -0500

    Mon 5/4/2020 update

--- covid_la.py
+++ covid_la.py
@@ -64,8 +64,11 @@ def la_covid(parish_url, state_url, capacity_url, la_tract_url, parish_deaths_ur
     cases = pd.DataFrame(esri_cleaner(parish_url))
     cases = cases.rename(columns = {'PFIPS' : 'FIPS'})
     cases.loc[cases['FIPS'] == '0', 'FIPS'] = '22000'
+    
     deaths = cases.copy()
+    
     tests_detail = cases.copy()
+    
     cases = cases.rename(columns = {'Cases' : date})
     case_file = pd.read_csv('data/cases.csv', dtype = {'FIPS' : object})
     if date in case_file.columns:
@@ -81,14 +84,16 @@ def la_covid(parish_url, state_url, capacity_url, la_tract_url, parish_deaths_ur
     death_file.merge(deaths[['FIPS', date]],
                       on='FIPS',
                       how='outer').to_csv('data/deaths.csv', index=False)
+    
     tracts = pd.DataFrame(esri_cleaner(la_tract_url))
-    tracts = tracts.rename(columns = {'TractID_1' : 'FIPS', 'CaseCount' : date})
+    tracts = tracts.rename(columns = {'TractID' : 'FIPS', 'CaseCount' : date})
     tracts_file = pd.read_csv('data/tracts.csv', dtype = {'FIPS' : object})
     if date in tracts_file.columns:
         tracts_file = tracts_file.drop(columns = date)
     tracts_file.merge(tracts[['FIPS', date]],
                       on='FIPS',
                       how='outer').to_csv('data/tracts.csv', index=False)
+    
     deaths_parish = pd.DataFrame(esri_cleaner(parish_deaths_url))
     for c in deaths_parish.iloc[:, 6:13].columns:
         deaths_parish[c] = pd.to_numeric(deaths_parish[c], errors='coerce')
@@ -113,6 +118,7 @@ def la_covid(parish_url, state_url, capacity_url, la_tract_url, parish_deaths_ur
     deaths_parish_file.merge(deaths_parish[['FIPS', 'Race', date]], 
                              on=['FIPS', 'Race'], 
                              how='outer').to_csv('data/deaths_by_race_parish.csv', index=False)
+    
     deaths_region = pd.DataFrame(esri_cleaner(region_deaths_url))
     deaths_region = deaths_region.rename(columns = {'Deaths' : date})
     deaths_region_file = pd.read_csv('data/deaths_by_race_region.csv')
@@ -121,6 +127,7 @@ def la_covid(parish_url, state_url, capacity_url, la_tract_url, parish_deaths_ur
     deaths_region_file.merge(deaths_region[['LDH_Region', 'Race', date]],
                              on=['LDH_Region', 'Race'],
                              how='outer').to_csv('data/deaths_by_race_region.csv', index=False)
+    
     tests_detail_file = pd.read_csv('data/tests.csv', dtype={'FIPS' : object})
     if date in tests_detail_file.columns:
         tests_detail_file = tests_detail_file.drop(columns = date)
@@ -135,27 +142,21 @@ def la_covid(parish_url, state_url, capacity_url, la_tract_url, parish_deaths_ur
                             left_on=['FIPS', 'Category'], 
                             right_on=['FIPS', 'Category'], 
                             how='outer').to_csv('data/tests.csv', index=False)
+    
     state = pd.DataFrame(esri_cleaner(la_state_url))
-    # tests = state[state['Category'] == 'Test Completed'].rename(columns = ({'Value' : 'Public', 'Value2' : 'Private'}))
-    # print(tests.head())
-    # tests['date'] = update_date
-    # tests = tests[['date', 'Public', 'Private']].set_index('date').transpose().reset_index().rename(columns={'index' : 'Category'})
-    # test_file = pd.read_csv('data/tests.csv')
-    # if date in test_file.columns:
-    #     test_file = test_file.drop(columns = date)
-    # test_file.merge(tests[['Category', date]], 
-    #                 on='Category', 
-    #                 how='outer').to_csv('data/tests.csv', index=False)
+
     case_demo = state[state['Category'] != 'Test Completed'].rename(columns=({'Value' : date}))
     case_demo_file = pd.read_csv('data/case_demo.csv')
     if date in case_demo_file.columns:
         case_demo_file = case_demo_file.drop(columns = date)
     case_demo_file.merge(case_demo[['Category', date]], on='Category', how='outer').to_csv('data/case_demo.csv', index=False)
+    
     death_demo = state[state['Category'] != 'Test Completed'].rename(columns=({'Value2' : date}))
     death_demo_file = pd.read_csv('data/death_demo.csv')
     if date in death_demo_file.columns:
         death_demo_file = death_demo_file.drop(columns = date)
     death_demo_file.merge(death_demo[['Category', date]], on='Category', how='outer').to_csv('data/death_demo.csv', index=False)
+    
     capacity = pd.DataFrame(esri_cleaner(capacity_url))
     capacity = capacity.rename(columns = {'HospVent' : 'Ventilators',
                                           'Bed' : 'Hospital Beds'})
@@ -171,7 +172,7 @@ update_date = '{d.month}/{d.day}/{d.year}'.format(d=datetime.now())
 la_state_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/arcgis/rest/services/State_Level_Information_2/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=pjson'
 la_county_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/ArcGIS/rest/services/Cases_by_Parish/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=pjson'
 region_capacity_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/ArcGIS/rest/services/Louisiana_Vent_and_Bed_Report/FeatureServer/0/query?where=1%3D1&outFields=*&f=pjson'
-la_tract_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/ArcGIS/rest/services/LA_2010_Tracts_04262020_2/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
+la_tract_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/ArcGIS/rest/services/LA_2018_Tracts_05032020/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
 la_deaths_parish_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/ArcGIS/rest/services/Deaths_by_Race_by_Parish/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=pjson'
 la_deaths_region_url = 'https://services5.arcgis.com/O5K6bb5dZVZcTo5M/ArcGIS/rest/services/Deaths_by_Race_by_Region/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=pjson'
 
