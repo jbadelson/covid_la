9c6fe7b30426996a9a20c3f01634731655da3479
Jeff Adelson <jeff.adelson@gmail.com>
2021-11-15 08:40:14 -0600

    Mon 11/15/2021 update

--- covid_la.py
+++ covid_la.py
@@ -35,8 +35,8 @@ logger.addHandler(stdout_handler)
 with open(f'{module_path}/static_data.json') as f:
     static_data = json.load(f)
 
-#update_date = pd.to_datetime('2021-11-06')
-update_date = datetime.now()
+update_date = pd.to_datetime('2021-11-15')
+#update_date = datetime.now()
 if os.name == 'nt':
     update_date_string = update_date.strftime('%#m/%#d/%#Y')
 else:
