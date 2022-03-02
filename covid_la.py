5a15a1b7cc5cf417942530e989b7fa3c58000e62
Jeff Adelson <jeff.adelson@gmail.com>
2022-03-02 11:29:59 -0600

    Wed 03/02/2022 update

--- covid_la.py
+++ covid_la.py
@@ -35,7 +35,7 @@ logger.addHandler(stdout_handler)
 with open(f'{module_path}/static_data.json') as f:
     static_data = json.load(f)
 
-update_date = pd.to_datetime('2022-02-28')
+update_date = pd.to_datetime('2022-03-01')
 #update_date = datetime.now()
 if os.name == 'nt':
     update_date_string = update_date.strftime('%#m/%#d/%#Y')
